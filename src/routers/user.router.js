a2d89c11d645e341a8b155d263afd215bc95be9a
food0903 <phuclthg@gmail.com>
2023-11-29 00:15:57 -0800

    A WORKING CART WOOOHOOO

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -123,6 +123,9 @@ router.post("/addToCart", validateJwt, async (req, res) => {
     //TEST
 
     await cart.save();
+
+    cart = await CartModel.findOne({ user: userId }).populate("foodList.food");
+
     res.json(cart);
     console.log(cart);
   } catch (error) {
@@ -139,49 +142,45 @@ router.delete(
   async (req, res) => {
     const userId = req.auth.id; // Extract user ID from JWT
     const { foodId, quantity } = req.params;
-    console.log("request results:", req.params);
-
-    let newTotalPrice = 0;
-    let itemRemoved = false;
 
     try {
       // Find the user's cart
       let cart = await CartModel.findOne({ user: userId });
-
       if (!cart) {
-        // If no cart exists, send a response indicating that the item was not found
         return res.status(404).json({ message: "Cart not found" });
       }
 
       // Find the index of the item to be removed in the foodList
       const itemIndex = cart.foodList.findIndex((item) =>
-        item.food.equals(foodId)
+          item.food._id.toString() === foodId
       );
 
       if (itemIndex === -1) {
-        // If the item is not found in the cart, send a response indicating that the item was not found
         return res.status(404).json({ message: "Item not found in cart" });
       }
 
       // Remove the item from the foodList
-      if (cart.foodList[itemIndex].quantity > quantity) {
-        cart.foodList[itemIndex].quantity -= quantity;
+      if (cart.foodList[itemIndex].quantity > parseInt(quantity)) {
+        cart.foodList[itemIndex].quantity -= parseInt(quantity);
       } else {
         cart.foodList.splice(itemIndex, 1);
       }
 
-      // Calculate and update the totalPrice based on the remaining items
+      // Recalculate the totalPrice
       let newTotalPrice = 0;
       for (const item of cart.foodList) {
         const foodItem = await FoodModel.findById(item.food);
         newTotalPrice += foodItem.price * item.quantity;
       }
-
       cart.totalPrice = newTotalPrice;
-      // Save the updated cart
-      await cart.save();
 
-      // Send a response indicating that the item has been successfully removed
+      // Save the updated cart
+      await cart.save(); 
+
+      // Repopulate the food objects before sending the response
+      cart = await CartModel.findOne({ user: userId }).populate('foodList.food');
+
+      // Send the response
       res.json({ message: "Item removed from cart", cart });
     } catch (error) {
       console.error("Error removing item from cart:", error);
@@ -189,6 +188,8 @@ router.delete(
     }
   }
 );
+
+
 
 //post to create user
 router.post(
